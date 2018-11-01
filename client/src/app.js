abd855361e04165a19106fe6d60630a143327827
Henko <henkorabie99@gmail.com>
2018-11-01 23:33:47 +0200

    Added Get Summoer call on react and implemented api call to riot

--- client/src/app.js
+++ client/src/app.js
@@ -3,35 +3,31 @@ import './app.css';
 
 class App extends Component {
     state = {
-        response: '',
-        post: '',
-        responseToPost: '',
+        summonerName: 'BFY Meowington',
+        summoner: {
+            "id":null,
+            "accountId":null,
+            "name":'',
+            "profileIconId":null,
+            "revisionDate":null,
+            "summonerLevel":null
+        }
     };
 
-    componentDidMount() {
-        this.callApi()
-            .then(res => this.setState({ response: res.express }))
-            .catch(err => console.log(err));
-    }
+    getSummonerInfo = async e => {
+        e.preventDefault();     
+        
+        const encodedValue = encodeURIComponent(this.state.summonerName);   
 
-    callApi = async () => {
-        const response = await fetch('/api/hello');
-        const body = await response.json();
-        if (response.status !== 200) throw Error(body.message);
-        return body;
-    };
-
-    handleSubmit = async e => {
-        e.preventDefault();
-        const response = await fetch('/api/world', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({ post: this.state.post }),
-        });
-        const body = await response.text();
-        this.setState({ responseToPost: body });
+        const response = await fetch(`/api/summoner?name=${encodedValue}`);
+        
+        if (response.status !== 200) {
+            console.log("boo :(");
+            return;
+        }
+        
+        const body = await response.json();        
+        this.setState({ summoner: JSON.parse(body.summoner) });
     };
 
     render() {
@@ -48,19 +44,18 @@ class App extends Component {
                         Learn React
                     </a>
                 </header>
-                <p>{this.state.response}</p>
-                <form onSubmit={this.handleSubmit}>
+                <form onSubmit={this.getSummonerInfo}>
                     <p>
                         <strong>Post to Server:</strong>
                     </p>
                     <input
                         type="text"
-                        value={this.state.post}
-                        onChange={e => this.setState({ post: e.target.value })}
+                        value={this.state.summonerName}
+                        onChange={e => this.setState({ summonerName: e.target.value })}
                     />
                     <button type="submit">Submit</button>
                 </form>
-                <p>{this.state.responseToPost}</p>
+                <p>{this.state.summoner.name}</p>
             </div>
         );
     }
--- server.js
+++ server.js
@@ -2,19 +2,44 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 const app = express();
+const https = require('https');
+
 const port = process.env.PORT || 5000;
+const riotAPI = "na1.api.riotgames.com";
+const riotToken = process.env.riotToken;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // API calls
-app.get('/api/hello', (req, res) => {
-  res.send({ express: 'Hello From Express' });
+app.get('/api/summoner', (req, res) => {
+  
+  var options = {
+    host: riotAPI,
+    port: 443,
+    path: '/lol/summoner/v3/summoners/by-name/'+encodeURIComponent(req.query.name),
+    method: 'GET',
+    headers: {
+      'X-Riot-Token': riotToken
+    }
+  };
+
+  https.request(options, function(response) {
+    response.setEncoding('utf8');
+    response.on('data', function (chunk) {
+      res.send({ summoner: chunk });
+    });
+    response.on('error', (e) => {
+      console.error(e);
+      res.status(500).send({ error: "boo:(" });
+    });
+  }).end(); 
+
 });
 
-app.post('/api/world', (req, res) => {
+app.post('/api/summoner', (req, res) => {
   res.send(
-    `I received your POST request. This is what you sent me: ${req.body.post}`,
+    `I received your POST request. This is what you sent me: ${req.body.summoner}`,
   );
 });
 
